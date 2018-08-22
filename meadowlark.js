0ff5812ce78d7063d8c2276d7822a1e70cf936bb
FFF-Fox <FFF-Fox@protonmail.com>
2018-08-22 19:01:54 +0300

    unit testing added.

--- meadowlark.js
+++ meadowlark.js
@@ -34,7 +34,15 @@ app.get('/about', function(req, res){
     res.render('about', {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
-    });    
+    });
+});
+
+// Tours page routes
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
 });
 
 // Custom 404 page
