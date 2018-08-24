863577bcde25f498b19b224c98edd216f7557350
FFF-Fox <fff-fox@protonmail.com>
2018-08-24 14:46:17 +0300

    Added database connection.

--- meadowlark.js
+++ meadowlark.js
@@ -32,6 +32,26 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('cookie-parser')(credentials.cookieSecret));
 
 /**
+ * Database connection
+ */
+var mongoose = require('mongoose');
+var opts = {
+    server: {
+        socketOptions: { keepAlive: 1 }
+    }
+};
+switch (app.get('env')) {
+    case 'development':
+        mongoose.connect(credentials.mongo.development.connectionString, opts);
+        break;
+    case 'production':
+        mongoose.connect(credentials.mongo.production.connectionString, opts);
+        break;
+    default:
+        throw new Error('Unknown execution environment: ' + app.get('env'));
+}
+
+/**
  * Routes go here..
  */
 // Home page
